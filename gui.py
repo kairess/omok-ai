ec39d069b833147d702bf8efc13738a0845adcca
Taehee <kairess87@gmail.com>
2020-12-17 22:24:10 +0900

    improve gui

--- gui.py
+++ gui.py
@@ -8,29 +8,40 @@ model = load_model('models/20201213_202430.h5')
 app = Ursina()
 
 window.borderless = False
+window.color = color._50
 
 w, h = 20, 20
 camera.orthographic = True
 camera.fov = 23
 camera.position = (w//2, h//2)
 
-player = Entity(name='1', color=color.orange)
-cursor = Tooltip(player.name, color=player.color, origin=(0.6, 0.6), scale=2, enabled=True)
-cursor.background.color = color.clear
+player = Entity(name='1', color=color.black)
 
 board = Board(w=w, h=h)
 board_buttons = [[None for x in range(w)] for y in range(h)]
 game = Gomoku(board=board)
 
+Entity(model=Grid(w+1, h+1), scale=w+1, color=color.black, x=w//2-0.5, y=h//2-0.5, z=0.1)
+
 for y in range(h):
     for x in range(w):
-        b = Button(parent=scene, position=(x, y)) # start from bottom left
+        b = Button(parent=scene, position=(x, y), color=color.clear, model='circle', scale=0.9) # start from bottom left
         board_buttons[y][x] = b
+
+        def on_mouse_enter(b=b):
+            if b.collision:
+                b.color = color._100
+        def on_mouse_exit(b=b):
+            if b.collision:
+                b.color = color.clear
+
+        b.on_mouse_enter = on_mouse_enter
+        b.on_mouse_exit = on_mouse_exit
 
         def on_click(b=b):
             # player turn
             b.text = '1'
-            b.color = color.orange
+            b.color = color.black
             b.collision = False
 
             game.put(x=int(b.position.x), y=int(h - b.position.y - 1)) # start from top left
@@ -53,9 +64,10 @@ for y in range(h):
             output_y, output_x = np.unravel_index(np.argmax(output), output.shape)
 
             game.put(x=output_x, y=output_y)
-            
+
             board_buttons[h - output_y - 1][output_x].text = '2'
-            board_buttons[h - output_y - 1][output_x].color = color.azure
+            board_buttons[h - output_y - 1][output_x].text_color = color.black
+            board_buttons[h - output_y - 1][output_x].color = color.white
             board_buttons[h - output_y - 1][output_x].collision = False
 
             won_player = game.check_won()
@@ -70,7 +82,6 @@ for y in range(h):
         b.on_click = on_click
 
 def end_session(won_player):
-    destroy(cursor)
     Panel(z=1, scale=10, model='quad')
     t = Text(f'Player {won_player} won!', scale=3, origin=(0, 0), background=True)
     t.create_background(padding=(.5,.25), radius=Text.size/2)
